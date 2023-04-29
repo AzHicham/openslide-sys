540c9d9991ff1ed66b741ce5a2e3da85020fe1e2
Hicham <hicham.azimani@owkin.com>
2023-04-29 10:56:21 +0200

    feat: Binding from installed openslide library

--- build.rs
+++ build.rs
@@ -1,15 +1,5 @@
 use std::{env, path::Path};
 
-#[cfg(feature = "dynamic-link")]
-fn statik_link() -> bool {
-    false
-}
-
-#[cfg(not(feature = "dynamic-link"))]
-fn statik_link() -> bool {
-    true
-}
-
 fn probe(s: &str) -> pkg_config::Library {
     pkg_config::Config::new()
         .cargo_metadata(false)
@@ -17,83 +7,28 @@ fn probe(s: &str) -> pkg_config::Library {
         .unwrap()
 }
 
-fn link_library(s: &str) {
-    pkg_config::Config::new()
-        .statik(statik_link())
-        .probe(s)
-        .unwrap();
-}
-
 fn main() {
-    let bindings = bindgen::Builder::default()
-        .header("src/openslide-sys/openslide.h")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("bindings.rs");
-    bindings
-        .write_to_file(dest_path)
-        .expect("Couldn't write bindings!");
-
     if env::var("DOCS_RS").is_err() {
-        let glib2 = probe("glib-2.0");
-        let cairo = probe("cairo");
-        let openjpeg = probe("libopenjp2");
-        let xml = probe("libxml-2.0");
-        let pixbuf = probe("gdk-pixbuf-2.0");
-        let png = probe("libpng16");
+        let libopenslide = probe("openslide"); // include_paths
 
-        cc::Build::new()
-            .include("src/openslide-sys")
-            .includes(glib2.include_paths)
-            .includes(cairo.include_paths)
-            .includes(openjpeg.include_paths)
-            .includes(xml.include_paths)
-            .includes(pixbuf.include_paths)
-            .includes(png.include_paths)
-            .file("src/openslide-sys/openslide-cache.c")
-            .file("src/openslide-sys/openslide-decode-gdkpixbuf.c")
-            .file("src/openslide-sys/openslide-decode-jp2k.c")
-            .file("src/openslide-sys/openslide-decode-jpeg.c")
-            .file("src/openslide-sys/openslide-decode-png.c")
-            .file("src/openslide-sys/openslide-decode-sqlite.c")
-            .file("src/openslide-sys/openslide-decode-tiff.c")
-            .file("src/openslide-sys/openslide-decode-tifflike.c")
-            .file("src/openslide-sys/openslide-decode-xml.c")
-            .file("src/openslide-sys/openslide-error.c")
-            .file("src/openslide-sys/openslide-grid.c")
-            .file("src/openslide-sys/openslide-hash.c")
-            .file("src/openslide-sys/openslide-jdatasrc.c")
-            .file("src/openslide-sys/openslide-tables.c")
-            .file("src/openslide-sys/openslide-util.c")
-            .file("src/openslide-sys/openslide-vendor-aperio.c")
-            .file("src/openslide-sys/openslide-vendor-generic-tiff.c")
-            .file("src/openslide-sys/openslide-vendor-hamamatsu.c")
-            .file("src/openslide-sys/openslide-vendor-leica.c")
-            .file("src/openslide-sys/openslide-vendor-mirax.c")
-            .file("src/openslide-sys/openslide-vendor-philips.c")
-            .file("src/openslide-sys/openslide-vendor-sakura.c")
-            .file("src/openslide-sys/openslide-vendor-trestle.c")
-            .file("src/openslide-sys/openslide-vendor-ventana.c")
-            .file("src/openslide-sys/openslide.c")
-            .define("GLIB_DISABLE_DEPRECATION_WARNINGS", None)
-            .compile("libopenslide.a");
+        let Some(include_dir) = libopenslide.include_paths.get(0) else {
+            panic!("Could not find include path for openslide")
+        };
 
-        link_library("gdk-pixbuf-2.0");
-        link_library("cairo");
-        link_library("libopenjp2");
-        link_library("libxml-2.0");
-        link_library("libpng16");
-        link_library("libtiff-4");
-        link_library("libjpeg");
-        link_library("sqlite3");
-        link_library("gio-2.0");
-        link_library("glib-2.0");
+        let bindings = bindgen::Builder::default()
+            .header(include_dir.join("openslide.h").to_string_lossy())
+            // Tell cargo to invalidate the built crate whenever any of the
+            // included header files changed.
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            // Finish the builder and generate the bindings.
+            .generate()
+            // Unwrap the Result and panic on failure.
+            .expect("Unable to generate bindings");
+
+        let out_dir = env::var_os("OUT_DIR").unwrap();
+        let dest_path = Path::new(&out_dir).join("bindings.rs");
+        bindings
+            .write_to_file(dest_path)
+            .expect("Couldn't write bindings!");
     }
 }
